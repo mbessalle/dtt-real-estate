42075bd0918434f2763e798d858cd3a0daf97bc6
mbessalle <mbessalle@gmail.com>
2023-03-05 10:39:10 +0100

    updated store to fetch houses API data

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,33 @@
 import { createStore } from 'vuex';
+import axios from 'axios';
 
 export default createStore({
   state: {
+    houses: [],
   },
   getters: {
+
   },
   mutations: {
+    setHouses(state, houses) {
+      state.houses = houses;
+    }
   },
   actions: {
+    async fetchHouses({ commit }) {
+      const config = {
+        method: 'get',
+        headers: {
+          'X-Api-Key': 'LIhlSFou52fiaEUHVKYXnQT1NC8bdrBM',}
+        }
+        try {
+          const response = await axios.get('https://api.intern.d-tt.nl/api/houses', config);
+          commit('setHouses', response.data);
+        }
+        catch (error) {
+          console.log('Error fetching data from the API',error);
+        }
+    }
   },
   modules: {
   },
