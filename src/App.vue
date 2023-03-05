e0cd4bd2250f338374de9d67de5e4157e3f1ccdf
mbessalle <mbessalle@gmail.com>
2023-03-05 09:43:36 +0100

    added some css

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <template>
-  <nav>
-    <router-link to="/" exact-active-class="active">Houses</router-link> |
-    <router-link to="/about" exact-active-class="active">About</router-link>
+  <nav class="navbar">
+    <div class="navbar-container">
+      <router-link to="/" exact-active-class="active">Houses</router-link> |
+      <router-link to="/about" exact-active-class="active">About</router-link>
+    </div>
   </nav>
-  <router-view/>
+  <router-view />
 </template>
 
 <style>
@@ -15,18 +17,27 @@
   color: #2c3e50;
 }
 
+.navbar-container {
+  max-width: 160px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-between;
+  align-items: left;
+  height: 64px;
+  padding: 0 20px;
+}
+
 nav {
   padding: 30px;
 }
 
 nav a {
   font-weight: bold;
-  color: #cdcfd2;
+  color: #a4a8ad;
 }
 
 nav .active {
   font-weight: bold;
   color: #090909;
 }
-
 </style>
