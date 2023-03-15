ec8eb034f892f7e88629a9732ce7d2c41f102cf3
mbessalle <mbessalle@gmail.com>
2023-03-15 15:51:02 +0100

    created client-side post newForm and server.js

--- src/components/NewForm.vue
+++ src/components/NewForm.vue
@@ -1,120 +1,139 @@
 <template>
-    <form @submit.prevent="createListing">
-      <label>
-        Image:
-        <input type="text" v-model="image" required />
-      </label>
-      <label>
-        Price:
-        <input type="number" v-model.number="price" required />
-      </label>
-      <label>
-        Number of bedrooms:
-        <input type="number" v-model.number="bedrooms" required />
-      </label>
-      <label>
-        Number of bathrooms:
-        <input type="number" v-model.number="bathrooms" required />
-      </label>
-      <label>
-        Size:
-        <input type="number" v-model.number="size" required />
-      </label>
-      <label>
-        Description:
-        <textarea v-model="description" required></textarea>
-      </label>
-      <label>
-        Street name:
-        <input type="text" v-model="street" required />
-      </label>
-      <label>
-        House number:
-        <input type="text" v-model="houseNumber" required />
-      </label>
-      <label>
-        Addition:
-        <input type="text" v-model="addition" />
-      </label>
-      <label>
-        Postal Code:
-        <input type="text" v-model="postalCode" required />
-      </label>
-      <label>
-        City:
-        <input type="text" v-model="city" required />
-      </label>
-      <label>
-        Construction year:
-        <input type="number" v-model.number="constructionYear" required />
-      </label>
-      <label>
-        Has garage:
-        <input type="checkbox" v-model="hasGarage" />
-      </label>
-      <button type="submit">Create Listing</button>
-    </form>
-  </template>
+  <form @submit.prevent="createListing">
+    <label>
+      Image:
+      <input type="file" @change="onFileChange($event)" />
+    </label>
+    <label>
+      Price:
+      <input type="number" v-model.number="price" required />
+    </label>
+    <label>
+      Number of bedrooms:
+      <input type="number" v-model.number="bedrooms" required />
+    </label>
+    <label>
+      Number of bathrooms:
+      <input type="number" v-model.number="bathrooms" required />
+    </label>
+    <label>
+      Size:
+      <input type="number" v-model.number="size" required />
+    </label>
+    <label>
+      Description:
+      <textarea v-model="description" required></textarea>
+    </label>
+    <label>
+      Street name:
+      <input type="text" v-model="street" required />
+    </label>
+    <label>
+      House number:
+      <input type="text" v-model="houseNumber" required />
+    </label>
+    <label>
+      Addition:
+      <input type="text" v-model="addition" />
+    </label>
+    <label>
+      Postal Code:
+      <input type="text" v-model="postalCode" required />
+    </label>
+    <label>
+      City:
+      <input type="text" v-model="city" required />
+    </label>
+    <label>
+      Construction year:
+      <input type="number" v-model.number="constructionYear" required />
+    </label>
+    <label>
+      Has garage:
+      <input type="checkbox" v-model="hasGarage" />
+    </label>
+    <button type="submit">Create Listing</button>
+  </form>
+</template>
   
 <script>
-  export default {
-    data() {
-      return {
-        image: '',
-        price: '',
-        bedrooms: '',
-        bathrooms: '',
-        size: '',
-        description: '',
-        street: '',
-        houseNumber: '',
-        addition: '',
-        postalCode: '',
-        city: '',
-        constructionYear: '',
-        hasGarage: false,
-      }
-    },
-    methods: {
-      createListing() {
-        // Validate form
-        if (!this.image || !this.price || !this.bedrooms || !this.bathrooms || !this.size || !this.description || !this.street || !this.houseNumber || !this.postalCode || !this.city || !this.constructionYear) {
-          alert('Please fill in all required fields.')
-          return
-        }
-  
-        // Create new listing object
-        const newListing = {
-          id: Math.floor(Math.random() * 100000) + 1, // Generate random id
-          image: this.image,
-          price: this.price,
-          rooms: {
-            bedrooms: this.bedrooms,
-            bathrooms: this.bathrooms,
-          },
-          size: this.size,
-          description: this.description,
-          location: {
-            street: this.street,
-            houseNumber: this.houseNumber,
-            addition: this.addition,
-            postalCode: this.postalCode,
-            city: this.city,
-          },
-          createdAt: new Date().toISOString().slice(0, 10), // Get current date in YYYY-MM-DD format
-          constructionYear: this.constructionYear,
-          hasGarage: this.hasGarage,
-          madeByMe: true,
-        }
-  
-        // Emit event to parent component to save new listing
-        this.$emit('create-listing', newListing)
-      }
+export default {
+  data() {
+    return {
+      image: null,
+      price: '',
+      bedrooms: '',
+      bathrooms: '',
+      size: '',
+      description: '',
+      street: '',
+      houseNumber: '',
+      addition: '',
+      postalCode: '',
+      city: '',
+      constructionYear: '',
+      hasGarage: false,
+      selectedFile: null,
     }
-  }
-  </script>
+  },
+  methods: {
+    async createListing() {
+      // Validate form
+      if (!this.image || !this.price || !this.bedrooms || !this.bathrooms || !this.size || !this.description || !this.street || !this.houseNumber || !this.postalCode || !this.city || !this.constructionYear) {
+        alert('Please fill in all required fields.')
+        return
+      }
 
-  <style>
+      // Create new data object
+      let data = new FormData();
+
+      data.append('image', this.image);
+      data.append('price', this.price);
+      data.append('bedrooms', this.bedrooms);
+      data.append('bathrooms', this.bathrooms);
+      data.append('size', this.size);
+      data.append('description', this.description);
+      data.append('street', this.street);
+      data.append('houseNumber', this.houseNumber);
+      data.append('addition', this.addition);
+      data.append('postalCode', this.postalCode);
+      data.append('city', this.city);
+      data.append('constructionYear', this.constructionYear);
+      data.append('hasGarage', this.hasGarage);
+      data.append('madeByMe', true);
+
+      // Add new house to the store
+      await this.$store.dispatch('createHouse', data)
+
+      // Reset form fields
+      this.image = null;
+      this.price = '';
+      this.bedrooms = '';
+      this.bathrooms = '';
+      this.size = '';
+      this.description = '';
+      this.street = '';
+      this.houseNumber = '';
+      this.addition = '';
+      this.postalCode = '';
+      this.city = '';
+      this.constructionYear = '';
+      this.hasGarage = false;
+    },
+    onFileChange(event) {
+      const file = event
+        .target.files[0]; // Get the first file uploaded
+      // Check if a file is uploaded
+      if (file) {
+        this.image = file;
+      };
+    },
+  }
+
+}
+</script>
+
+<style>
 /* Set font and background color */
 body {
   font-family: Arial, sans-serif;
@@ -132,13 +151,16 @@ body {
 }
 
 /* Style the input field */
-input[type="text"], 
-input[type="email"], 
+input[type="text"],
+input[type="email"],
 textarea {
-  width: 70%; /* changed width from 100% to 70% */
-  max-width: 400px; /* added max-width to limit the width of the input fields */
+  width: 70%;
+  /* changed width from 100% to 70% */
+  max-width: 400px;
+  /* added max-width to limit the width of the input fields */
   padding: 12px 20px;
-  margin: 8px auto; /* changed margin to center the input fields */
+  margin: 8px auto;
+  /* changed margin to center the input fields */
   box-sizing: border-box;
   border: none;
   border-radius: 4px;
@@ -166,6 +188,4 @@ label {
   font-weight: bold;
   margin-bottom: 5px;
 }
-
-
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,6 @@
 const { createStore } = require("vuex");
 const axios = require("axios");
+const FormData = require("form-data");
 
 module.exports = createStore({
   state: {
@@ -14,8 +15,11 @@ module.exports = createStore({
     },
     sortByPrice(state, sortOrder) {
       state.houses.sort((a, b) => {
-        return sortOrder === 'asc' ? a.price - b.price : b.price - a.price;
+        return sortOrder === "asc" ? a.price - b.price : b.price - a.price;
       });
+    },
+    addHouse(state, house) {
+      state.houses.push(house);
     },
   },
   actions: {
@@ -27,7 +31,7 @@ module.exports = createStore({
         },
       };
       try {
-        const response = await axios.get("https://api.intern.d-tt.nl/api/houses", config);
+        const response = await axios.get("/api/houses", config);
         commit("setHouses", response.data);
         console.log("Houses fetched from the API", JSON.stringify(response.data));
       } catch (error) {
@@ -35,12 +39,48 @@ module.exports = createStore({
       }
     },
     filterHouses({ commit, state }, location) {
-      const filteredHouses = state.houses.filter(house => house.location === location);
+      const filteredHouses = state.houses.filter((house) => house.location === location);
       commit("setHouses", filteredHouses);
     },
-    sortByPrice({ commit, state }, sortOrder) {
+    sortByPrice({ commit }, sortOrder) {
       commit("sortByPrice", sortOrder);
     },
+    async createHouse({ commit })
+     {
+      try {
+        let data = new FormData();
+        data.append('price', '20');
+        data.append('bedrooms', '1');
+        data.append('bathrooms', '1');
+        data.append('size', '1');
+        data.append('streetName', 'Overtoom');
+        data.append('houseNumber', '21');
+        data.append('numberAddition', '1');
+        data.append('zip', '1181TY');
+        data.append('city', 'Amsterdam');
+        data.append('constructionYear', '1960');
+        data.append('hasGarage', 'false');
+        data.append('description', 'Nice house!');
+
+        const config = {
+          method: "post",
+          maxBodyLength: Infinity,
+          url: "https://api.intern.d-tt.nl/api/houses",
+          headers: {
+            "X-Api-Key": "LIhlSFou52fiaEUHVKYXnQT1NC8bdrBM",
+            // ...data.getHeaders()  // 
+          },
+          data: data,
+        };
+
+        const response = await axios(config);
+        console.log("New house added:", JSON.stringify(response.data));
+        commit("addHouse", response.data);
+      } catch (error) {
+        console.log("Error creating new house", error);
+      }
+    },
   },
+
   modules: {},
 });
