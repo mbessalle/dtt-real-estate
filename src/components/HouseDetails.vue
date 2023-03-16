18a815b1fe42bbeb6dc7ba504b0b2af6e3b3b4e6
mbessalle <mbessalle@gmail.com>
2023-03-16 21:19:33 +0100

    fixed HouseDetails.vue

--- src/components/HouseDetails.vue
+++ src/components/HouseDetails.vue
@@ -1,36 +1,38 @@
 <template>
-    <div v-if="house" class="house-details">
-      <img v-if="house.image" :src="house.image" alt="House image">
-      <p v-else>No image available</p>
-      <h2>Address: {{ house.location.street }}, {{ house.location.city }}, {{ house.location.country }}</h2>
-      <p>Postal code: {{ house.location.zip }}</p>
-      <p>Description: {{ house.description }}</p>
-      <p>Number of bedrooms: {{ house.rooms.bedrooms }}</p>
-      <p>Number of bathrooms: {{ house.rooms.bathrooms }}</p>
-      <p>Size: {{ house.size }} mts2</p>
-      <p>Construction year: {{ house.constructionYear }}</p>
-      <p>Garage: {{ house.hasGarage ? 'Yes' : 'No' }}</p>
-    </div>
-<div v-else>
-        <p>House not found. Please reload main page</p>
-        </div>
-  </template>
+  <div v-if="house" class="house-details">
+    <img v-if="house.image" :src="house.image" alt="House image">
+    <p v-else>No image available</p>
+    <h2>Address: {{ house.location.street }}, {{ house.location.city }}, {{ house.location.country }}</h2>
+    <p>Postal code: {{ house.location.zip }}</p>
+    <p>Description: {{ house.description }}</p>
+    <p>Number of bedrooms: {{ house.rooms.bedrooms }}</p>
+    <p>Number of bathrooms: {{ house.rooms.bathrooms }}</p>
+    <p>Size: {{ house.size }} mts2</p>
+    <p>Construction year: {{ house.constructionYear }}</p>
+    <p>Garage: {{ house.hasGarage ? 'Yes' : 'No' }}</p>
+  </div>
+  <div v-else>
+    <p>House not found. Please reload main page</p>
+  </div>
+</template>
   
-  <script>
-  import { mapState } from 'vuex';
-  export default {
+<script>
+import { mapState } from 'vuex';
+export default {
   name: "HouseDetails",
   props: {
     id: {
-      type: String,
+      type: Number,
       required: true
     }
   },
   computed: {
     ...mapState(["houses"]),
     house() {
-      const index = this.id - 2; // subtract 2 to account for 0-based indexing and the fact that the first house has id 2
-      return this.houses[index];
+      if (this.id) {
+        return this.houses.find(house => house.id === Number(this.id));
+      }
+      return null;
     }
   },
 };
@@ -39,22 +41,22 @@
   
 <style scoped>
 .house-details {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    gap: 20px;
-    margin-bottom: 20px;
-    justify-content: center;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  margin-bottom: 20px;
+  justify-content: center;
 }
 
 img {
-    width: 200px;
-    height: 200px;
-    align-self: center;
+  width: 200px;
+  height: 200px;
+  align-self: center;
 }
 
 .house-details p {
-    margin: 0;
+  margin: 0;
 }
 </style>
   
